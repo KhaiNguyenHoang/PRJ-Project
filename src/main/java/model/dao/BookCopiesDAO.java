d0cf431c1de16d7d058e7b09f061cdd48aa2d6c4
Nguyễn Hoàng Khải <khaiproject1234@gmail.com>
2025-02-24 20:02:41 +0700

    Merge remote-tracking branch 'origin/master' update DAO BookCopiesDAO.java

--- src/main/java/model/dao/BookCopiesDAO.java
+++ src/main/java/model/dao/BookCopiesDAO.java
@@ -1,9 +1,121 @@
 package model.dao;
 
+import model.entity.BookCopies;
 import model.utils.LibraryContext;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BookCopiesDAO extends LibraryContext {
     public BookCopiesDAO() {
         super();
+    }
+
+    public List<BookCopies> getAllCopies() {
+        List<BookCopies> copies = new ArrayList<>();
+        try {
+            String query = "SELECT IdCopy, BookID, CopyNumber, Available FROM BookCopies";
+            PreparedStatement statement = conn.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int idCopy = resultSet.getInt("IdCopy");
+                int bookId = resultSet.getInt("BookID");
+                int copyNumber = resultSet.getInt("CopyNumber");
+                String status = resultSet.getString("Status");
+                BookCopies copy = new BookCopies(idCopy, bookId, copyNumber, status);
+                copies.add(copy);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BookCopiesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return copies;
+    }
+
+    public boolean addCopy(BookCopies copy) {
+        try {
+            String query = "INSERT INTO BookCopies (BookID, CopyNumber, Status) VALUES (?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setInt(1, copy.getBookId());
+            statement.setInt(2, copy.getCopyNumber());
+            statement.setString(3, copy.getStatus());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(BookCopiesDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean updateCopy(BookCopies copy) {
+        try {
+            String query = "UPDATE BookCopies SET BookID = ?, CopyNumber = ?, Status = ? WHERE IdCopy = ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setInt(1, copy.getBookId());
+            statement.setInt(2, copy.getCopyNumber());
+            statement.setString(3, copy.getStatus());
+            statement.setInt(4, copy.getIdCopy());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(BookCopiesDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean deleteCopy(int idCopy) {
+        try {
+            String query = "DELETE FROM BookCopies WHERE IdCopy = ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setInt(1, idCopy);
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(BookCopiesDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public BookCopies getCopyById(int idCopy) {
+        BookCopies copy = null;
+        try {
+            String query = "SELECT IdCopy, BookID, CopyNumber, Status FROM BookCopies WHERE IdCopy = ?";
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setInt(1, idCopy);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                int bookId = resultSet.getInt("BookID");
+                int copyNumber = resultSet.getInt("CopyNumber");
+                String status = resultSet.getString("Status");
+                copy = new BookCopies(idCopy, bookId, copyNumber, status);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BookCopiesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return copy;
+    }
+
+    public List<BookCopies> getAvailableCopies() {
+        List<BookCopies> copies = new ArrayList<>();
+        try {
+            String query = "SELECT IdCopy, BookID, CopyNumber, Status FROM BookCopies WHERE Status = 'Available'";
+            PreparedStatement statement = conn.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int idCopy = resultSet.getInt("IdCopy");
+                int bookId = resultSet.getInt("BookID");
+                int copyNumber = resultSet.getInt("CopyNumber");
+                String status = resultSet.getString("Status");
+                BookCopies copy = new BookCopies(idCopy, bookId, copyNumber, status);
+                copies.add(copy);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BookCopiesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return copies;
     }
 }
